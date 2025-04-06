cb1dd6b8c54e3417423b5536140dab3f9768ad56
MichalAniol258 <146379528+MichalAniol258@users.noreply.github.com>
2025-04-06 22:08:14 +0200

    2slajd

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Card({ h1, p, img }: { h1: string, p: string, img: string }) {
     return (
-        <div className="p-[3px] rounded-2xl bg-gradient-to-br from-yellow-200 to-blue-200 max-w-xs w-full z-1">
+        <div className="p-[3px] rounded-2xl bg-gradient-to-br from-yellow-200 to-blue-200 max-w-xs w-full z-1 ">
             <div className="bg-gray-950 rounded-2xl shadow-md overflow-hidden">
                 <Image
                     className="w-full h-48 object-cover opacity-60"
@@ -12,13 +12,13 @@ export default function Card({ h1, p, img }: { h1: string, p: string, img: strin
                     unoptimized
                     alt="Card image"
                 />
-                <div className="p-6 flex flex-col justify-between">
+                <div className="p-6 flex flex-col justify-between h-[225px]">
 
                     <h2 className="text-xl font-semibold text-yellow-200 mb-2">{h1}</h2>
                     <p className="text-gray-300 text-sm flex-grow">{p}</p>
 
                     <button className="mt-4 inline-block px-4 py-2 bg-yellow-200 text-gray-950 text-sm font-medium rounded-lg hover:bg-blue-200 transition-colors">
-                        Action
+                        Github
                     </button>
                 </div>
             </div>
