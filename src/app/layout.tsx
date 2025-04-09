73dee3ddcea4e07e558d4efcca2ec184c700bd27
MichalAniol258 <146379528+MichalAniol258@users.noreply.github.com>
2025-04-09 20:50:17 +0200

    logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,10 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "MichalAniol - Portfolio",
   description: "Tutaj doszlo do sigmy",
+  icons: {
+    icon: "/logo/logo.ico", // Główna ikona
+    apple: "/logo/apple-touch-logo.png", // Ikona dla iOS
+  },
 };
 
 
@@ -23,6 +27,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo/logo-16x16.png" sizes="16x16" />
+        <link rel="icon" href="/logo/logo-32x32.png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/logo/apple-touch-logo.png" sizes="180x180" />
+        <link rel="icon" href="/logo/android-chrome-192x192.png" sizes="192x192" />
+      </head>
 
       <body
         className={`${inter.variable}  antialiased bg-gray-950`}
