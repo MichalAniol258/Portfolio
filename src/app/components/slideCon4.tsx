c31bb6c76260cecf22d49e0df47f2414da076788
MichalAniol258 <146379528+MichalAniol258@users.noreply.github.com>
2025-04-11 20:44:32 +0200

    Update slideCon4.tsx

--- src/app/components/slideCon4.tsx
+++ src/app/components/slideCon4.tsx
@@ -31,21 +31,21 @@ export default function SlideCon4() {
 
           <div className="grid grid-flow-row-dense grid-cols-3 max-sm:grid-cols-2 gap-4 mt-8 z-10 opacity-85">
             <Link
-              className="bg-gradient-to-r from-[#ffdd6e] to-[#ff7f50] text-white font-bold py-2 px-6 rounded-lg shadow-md hover:scale-105 transition transform text-center"
+              className="bg-gradient-to-r from-[#ffdd6e] text-nowrap to-[#ff7f50] text-white font-bold py-2 px-6 rounded-lg shadow-md hover:scale-105 transition transform text-center"
               href="https://github.com/MichalAniol258"
               target="_blank"
             >
               GitHub
             </Link>
             <Link
-              className="bg-gradient-to-r from-[#1DA1F2] to-[#0076B6] text-white font-bold py-2 px-6 rounded-lg shadow-md hover:scale-105 transition transform text-center"
+              className="bg-gradient-to-r from-[#1DA1F2] text-nowrap to-[#0076B6] text-white font-bold py-2 px-6 rounded-lg shadow-md hover:scale-105 transition transform text-center"
               href="#"
               target="_blank"
             >
               Twitter(not yet)
             </Link>
             <Link
-              className="bg-gradient-to-r from-[#4B6A92] to-[#2A3E57] text-white font-bold py-2 px-6 rounded-lg shadow-md hover:scale-105 transition transform text-center"
+              className="bg-gradient-to-r from-[#4B6A92] text-nowrap to-[#2A3E57] text-white font-bold py-2 px-6 rounded-lg shadow-md hover:scale-105 transition transform text-center"
               href="#"
               target="_blank"
             >
